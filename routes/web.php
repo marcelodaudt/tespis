62927242521a1dae79b06b8625868cd23c1e77be
Marcelo Daudt <mdaudt@yahoo.com.br>
2023-11-01 18:36:22 -0300

    Implementação de autenticação e do tema USP.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
