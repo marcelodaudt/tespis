05d8b68e6a395f585f31e7a85c17911af0f89dd2
Marcelo Daudt <mdaudt@yahoo.com.br>
2025-08-20 17:02:09 -0300

    Alterada a página principal do sistema.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+.caixa-destaque {
+    border: 2px solid #ccc;
+    padding: 10px;
+    margin-bottom: 10px;
+    background-color: #f9f9f9;
+    border-radius: 5px;
+    box-shadow: 2px 2px 5px #aaa;
+}
+</style>
+
 <h3>Em desenvolvimento</h3>
 
 <p>Sistema para Gestão Acadêmica.</p>
 
 <p>Realiza controle centralizado de alunos, professores, cursos, disciplinas, histórico escolar e turmas.</p>
+
+<br><br>
+
+<div class="caixa-destaque">
+    <p><strong>Téspis</strong> foi, segundo algumas fontes da <strong>Grécia Antiga</strong>, especialmente <strong>Aristóteles</strong>, um ator e poeta grego.
+    É amplamente conotado como o primeiro ator da história, tendo representado personagens em peças de teatro.
+    Segundo outras fontes, ele introduziu a figura do ator principal, destacando-se do coro.
+    É também considerado como o inventor da tragédia e o primeiro produtor teatral.
+    A palavra inglesa <i>thespian</i>, sinônimo de ator, é inspirada por <strong>Téspis</strong>.</p>
+    <p>Fonte: <a href="https://pt.wikipedia.org/wiki/T%C3%A9spis" target="_blank">Téspis - Wikipédia.</a></p>
+</div>
 
 @endsection
